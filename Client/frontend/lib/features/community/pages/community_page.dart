606e87841e7732c99ea54ca6c29dd2b2979a6b00
Hong-Layeang <layeang.hong@student.cadt.edu.kh>
2026-01-31 00:32:40 +0700

    Working on search page UI

--- Client/frontend/lib/features/community/pages/community_page.dart
+++ Client/frontend/lib/features/community/pages/community_page.dart
@@ -1,10 +1,12 @@
-﻿import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class CommunityPage extends StatelessWidget {
   const CommunityPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Community Page'));
+    return const Center(
+      child: Text('CommunityPage'),
+    );
   }
 }
